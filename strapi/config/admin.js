290b9d17a38f4850973ea28ce37c279bf976a5d7
Rade Despodovski <rade@microtica.com>
2022-08-10 16:30:38 +0300

    [strapi-serverless] add JWT secret and api token salt

--- strapi/config/admin.js
+++ strapi/config/admin.js
@@ -1,4 +1,7 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env('API_TOKEN_SALT', '8ztsp661bop8824j8umr96skg5kx79xb'),
+  },
   auth: {
     secret: env('ADMIN_JWT_SECRET', '025935686065ccf661d183c8180e8394'),
   },
