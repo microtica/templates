5451e8573206f70d56a3a1186b731d8bc2524e0b
Rade Despodovski <rade@microtica.com>
2022-09-06 15:50:24 +0200

    [strapi-serverless] upload assets on S3

--- strapi-serverless/config/plugins.js
+++ strapi-serverless/config/plugins.js
@@ -3,5 +3,21 @@ module.exports = ({ env }) => ({
         config: {
             jwtSecret: env('CUSTOM_JWT_SECRET', '720d41ccd82bad03552cdaf3101b0aa3acc4db3acaa3415871babf2f9c010839b68993439c23e6aecb2739b51b935ac649beb6bf29904ebdcaba9645e81abc22')
         }
+    },
+    "upload": {
+        config: {
+            provider: 'aws-s3',
+            providerOptions: {
+                region: env('AWS_REGION'),
+                params: {
+                    Bucket: env('AWS_BUCKET'),
+                },
+            },
+            actionOptions: {
+                upload: {},
+                uploadStream: {},
+                delete: {},
+            }
+        }
     }
 });  
