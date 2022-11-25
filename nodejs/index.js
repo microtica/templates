e4ef7c578fdbe08bff66f52d72c8ca2d97d045d3
Rade Despodovski <rade@microtica.com>
2022-11-25 17:03:35 +0100

    remove VPC component from templates (now it's auto-generated on template deployment)

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
